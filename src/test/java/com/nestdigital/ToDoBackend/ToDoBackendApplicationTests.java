9e646e88f82181fa4472a7540115ff1424db3519
Liju-07 <lijusaju03@gmail.com>
2022-10-25 14:36:33 +0530

    todo

---
+++ src/test/java/com/nestdigital/ToDoBackend/ToDoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.ToDoBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
